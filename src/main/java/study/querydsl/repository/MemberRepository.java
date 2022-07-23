d4106f8d67d60cc915e3b2224f19264d5f5e3ba6
wisehero10 <godjw96@gmail.com>
2022-07-23 15:43:47 +0900

    feat : 사용자 정의 메서드

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import study.querydsl.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 	List<Member> findByUsername(String username);
 }
